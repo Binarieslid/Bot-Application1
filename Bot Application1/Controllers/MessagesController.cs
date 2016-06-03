05c1990af943df9e6c374c845de4fb673bef8b33
Binarieslid <info@binarieslid.com>
2016-06-03 17:09:01 -0400

    Initial Commit

--- Bot Application1/Controllers/MessagesController.cs
+++ Bot Application1/Controllers/MessagesController.cs
@@ -6,12 +6,46 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
-using Microsoft.Bot.Connector.Utilities;
-
 using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Dialogs;
 
 namespace Bot_Application1
 {
+    [Serializable]
+    public class HelpDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(YourFunctionForMessageReceived);
+        }
+        public async Task YourFunctionForMessageReceived(IDialogContext context, IAwaitable<Message> argument)
+        {
+            var message = await argument;
+            if (message.Text == "help")
+            {
+                PromptDialog.Confirm(
+                    context,
+                    YourFunctionForHelpCommand,
+                    "Are you sure you want to display help?",
+                    "Didn't get that!");
+            }
+            else
+            {
+                int length = (message.Text ?? string.Empty).Length;
+                await context.PostAsync($"You sent {length} characters");
+                context.Wait(YourFunctionForMessageReceived);
+            }
+        }
+        public async Task YourFunctionForHelpCommand(IDialogContext context, IAwaitable<bool> argument)
+        {
+            var confirm = await argument;
+            if (confirm)
+            {
+                await context.PostAsync("I am a simple bot that will display the length of your messages! You can display this message by typing \"help\"!");
+            }
+            context.Wait(YourFunctionForMessageReceived);
+        }
+    }
     [BotAuthentication]
     public class MessagesController : ApiController
     {
@@ -23,29 +57,7 @@ namespace Bot_Application1
         {
             if (message.Type == "Message")
             {
-                // calculate something for us to return
-                //int length = (message.Text ?? string.Empty).Length;
-                var counter = message.GetBotPerUserInConversationData<int>("counter");
-
-                // save our new counter by adding it to the outgoing message
-                //
-                Message replyMessage = message;
-                if (message.Text == "reset")
-                {
-                    counter = 0;
-                    replyMessage = message.CreateReplyMessage($"{++counter} Counter has been reseted");
-                    replyMessage.SetBotPerUserInConversationData("counter", counter);
-                }
-                else
-                {
-                    replyMessage = message.CreateReplyMessage($"{++counter} You said:{message.Text}");
-                    replyMessage.SetBotPerUserInConversationData("counter", counter);
-                }
-                
-                //return message.CreateReplyMessage("Hey, how can I help you?","en");
-                //return null;
-                return replyMessage;
-
+                return await Conversation.SendAsync(message, () => new HelpDialog());
             }
             else
             {
@@ -65,29 +77,23 @@ namespace Bot_Application1
             {
                 // Implement user deletion here
                 // If we handle user deletion, return a real message
-                return message.CreateReplyMessage("User deleted from Conversation!");
             }
             else if (message.Type == "BotAddedToConversation")
             {
-                return message.CreateReplyMessage("Hello BOT!");
             }
             else if (message.Type == "BotRemovedFromConversation")
             {
-                return message.CreateReplyMessage("Bot Left Conversation!");
             }
             else if (message.Type == "UserAddedToConversation")
             {
-                return message.CreateReplyMessage("Hello User!");
             }
             else if (message.Type == "UserRemovedFromConversation")
             {
-                return message.CreateReplyMessage("User Out!");
             }
             else if (message.Type == "EndOfConversation")
             {
-                return message.CreateReplyMessage("Conversaton Ended!");
             }
-            
+
             return null;
         }
     }
